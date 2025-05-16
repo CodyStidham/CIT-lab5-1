91442ff43912f5beca9e299bc69e4832ed0dcd6f
CodyStidham <159469261+CodyStidham@users.noreply.github.com>
2025-05-15 23:15:32 -0400

    Add files via upload

---
+++ data-gen.py
@@ -0,0 +1,22 @@
+import sqlite3
+import os
+
+DATABASE = '/nfs/demo.db'
+
+def connect_db():
+    """Connect to the SQLite database."""
+    return sqlite3.connect(DATABASE)
+
+def generate_test_data(num_contacts):
+    """Generate test data for the contacts table."""
+    db = connect_db()
+    for i in range(num_contacts):
+        name = f'Test Name {i}'
+        email = f'stidhalt@miamioh.edu{i}'
+        db.execute('INSERT INTO contacts (name, email) VALUES (?, ?)', (name, email))
+    db.commit()
+    print(f'{num_contacts} test contacts added to the database.')
+    db.close()
+
+if __name__ == '__main__':
+    generate_test_data(10)  # Generate 10 test contacts.
